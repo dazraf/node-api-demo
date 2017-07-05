f1d7d9b601028649b011a8cd84d87fb271884d82
dazraf <farzad.pezeshkpour@gmail.com>
2017-07-05 13:39:51 +0100

    README. more end points. index.html docs updates

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 'use strict';
 
 const express = require('express');
-
+var path = require('path');
 const app = express();
 
-// [START hello_world]
-// Say hello!
+const wallets = {
+  "w1": {
+    "id": "w1",
+    "name": "Oliver",
+    "balance": 100000,
+    "account": "11223344",
+    "sortCode": "112233"
+  }
+};
+
+// retrieve intro screen
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, Oliver!');
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
-// [END hello_world]
+
+// retrieve existing wallets
+app.get('/wallets', (req, res) => {
+  sendJson(res, wallets)
+});
+
+app.get('/wallets/:wallet', (req, res) => {
+  var wallet = req.params.wallet;
+  if (wallet in wallets) {
+    sendJson(res, wallets[wallet])
+  } else {
+    sendError(res, "wallet not found: " + wallet)
+  }
+});
 
 if (module === require.main) {
   // [START server]
@@ -21,6 +43,15 @@ if (module === require.main) {
     console.log("browse to http://localhost:" + port);
   });
   // [END server]
+}
+
+function sendJson(res, obj) {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send(JSON.stringify(obj))
+}
+
+function sendError(res, msg) {
+  res.status(500, msg).send(msg)
 }
 
 module.exports = app;
